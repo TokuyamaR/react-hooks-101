4e3095b9814fa1676a168253d712fb7d293a3c2b
TokuyamaR <qpflapper.1007@gmail.com>
2021-03-06 11:16:37 +0900

    add multiple states and default props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const increment2 = () => setCount(previousCount => previousCount + 1);
-  const decrement2 = () => setCount(previousCount => previousCount - 1);
-  const reset = () => setCount(previousCount => 0);
-  const doubleCount = () => setCount(previousCount => previousCount * 2);
-  const devideThree = () => setCount(previousCount => devideThreeMultiple(previousCount));
-
-  const devideThreeMultiple = (num) => (num % 3 === 0) ? (num / 3) : num;
+  const reset = () => {
+    setName(props.name);
+    setPrice(props.price);
+  };
 
   return (
     <>
-      <div className="App">
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={doubleCount}>x2</button>
-      </div>
-      <div>
-        <button onClick={devideThree}>/3</button>
-      </div>
+      <p>
+        現在の{name}の価格は{price}円です
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)}></input>
     </>
   );
-}
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
+};
 
 export default App;
